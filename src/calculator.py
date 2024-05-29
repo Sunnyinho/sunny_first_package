848d26fe434ffeda76cfd73b0b2414cbd5a5d37d
Sunny Shah <sunny.shah@cloudfactory.com>
2024-05-29 17:18:46 +0545

    initial commit

---
+++ src/calculator.py
@@ -0,0 +1,20 @@
+class Calculator:
+    def __init__(self,  a, b):
+        self.a = a
+        self.b = b
+        self.addition()
+        self.subtraction()
+        self.multiplication()
+        self.division()
+
+    def addition(self):
+        return f"The addition value is: {self.a + self.b}"
+
+    def subtraction(self):
+        return f"The subtraction value is: {self.a - self.b}"
+
+    def multiplication(self):
+        return f"The multiplication value is: {self.a * self.b}"
+
+    def division(self):
+        return f"The division value is: {self.a / self.b}"
